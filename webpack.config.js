c395560ad6ff3cf817d6b984cdb46f67b46662d1
EmericReactJS <devemeric1983@gmail.com>
2020-06-20 18:35:37 +0200

    adds dependencies & webpack & babel config file

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require("path");
+const webpack = require("webpack");
+
+const rules = [
+  {
+    test: /\.(js|jsx)$/,
+    exclude: /node_modules/,
+    loader: "babel-loader",
+    options: { presets: ["@babel/env"] },
+  },
+  {
+    test: /\.css$/,
+    use: ["style-loader", "css-loader"],
+  },
+];
+
+const entry = path.join(__dirname, "src", "index.js");
+
+module.exports = {
+  mode: "development",
+  entry: ["react-hot-loader/patch", entry],
+  module: {
+    rules: rules,
+  },
+  resolve: {
+    extensions: ["*", ".js", ".jsx"],
+    alias: {
+      "react-dom": "@hot-loader/react-dom",
+    },
+  },
+  output: {
+    path: path.join(__dirname, "dist"),
+    filename: "bundle.js",
+    publicPath: "/dist/",
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "public/"),
+    port: 3000,
+    publicPath: "http://localhost:3000/dist/",
+    hotOnly: true,
+  },
+  plugins: [new webpack.HotModuleReplacementPlugin()],
+};
