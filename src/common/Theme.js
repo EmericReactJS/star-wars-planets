eaa6bb2df9023eab218e01e6de3416d1b29a9e4f
EmericReactJS <devemeric1983@gmail.com>
2020-06-21 12:15:21 +0200

    adds style management dependencies & theme

---
+++ src/common/Theme.js
@@ -0,0 +1,14 @@
+const colors = {
+  swpPrimaryBlue: "#001E62",
+  swpLightBlue: "#8B98B7",
+  swpPrimaryOrange: "#FE5000",
+  swpLightOrange: "#FEAF8B",
+  swpGrey: "#E7EAF0",
+  swpLightGrey: "#F0F1F2",
+};
+
+const fonts = {
+  primaryFont: "Lato",
+};
+
+export { colors, fonts };
